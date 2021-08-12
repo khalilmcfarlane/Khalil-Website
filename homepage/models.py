b353efc524d4aed67338e285142e67c503239e32
khalilmcfarlane <kmac36960@gmail.com>
2021-08-11 20:06:10 -0400

    Created models for homepage + resume page

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,14 @@
-# from django.db import models
+from django.db import models
+
 
 # Create your models here.
+class HomePage(models.Model):
+    title = models.CharField(max_length=50)
+    about_me_title = models.CharField(max_length=500)
+    about_me_description = models.TextField()
+    self_image = models.ImageField(height_field=15, width_field=15)
+    linked_in_url = models.URLField()
+    github_url = models.URLField()
+
+    def __str__(self):
+        return self.title
--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+from django.db import models
+
 
 # Create your models here.
+class Resume(models.Model):
+    resume_title = models.CharField()
+    resume_space = models.TextField()
+
+    def __str__(self):
+        return self.resume_title
